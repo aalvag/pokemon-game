1da7d93f9213792de10695ad6d4d23d9420a204f
David Alvarez <david.alvarez@incluit.com>
2023-05-16 10:56:22 -0300

    create components

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Picture</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonPicture",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Who is that Pokémon?</h1>
+  <PokemonPicture />
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import PokemonPicture from "@/components/PokemonPicture.vue";
+
+export default {
+  name: "PokemonPage",
+  components: { PokemonPicture, PokemonOptions },
+};
+</script>
+
+<style lang="scss" scoped></style>
