8f43fb5f32b7ac52b989bcbf30dbe1ee5906625f
David Alvarez <david.alvarez@incluit.com>
2023-05-16 11:15:26 -0300

    desing components

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,14 +1,41 @@
 <template>
-  <div>
-    <h2>Options</h2>
+  <div class="options-container">
+    <ul>
+      <li>Option 1</li>
+      <li>Option 2</li>
+      <li>Option 3</li>
+      <li>Option 4</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "",
+  name: "PokemonOptions",
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style-type: none;
+}
+
+li {
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 10px;
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 250px;
+}
+
+li:hover {
+  background-color: #f1f1f1;
+}
+
+.options-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
 
--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <h2>Picture</h2>
+  <div class="pokemon-container">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+      alt="pokemon"
+      class="hidden-pokemon"
+    />
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+      alt="pokemon"
+      class="fade-in"
+    />
   </div>
 </template>
 
@@ -10,4 +19,21 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+img {
+  height: 200px;
+  position: absolute;
+  right: 32%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+</style>
